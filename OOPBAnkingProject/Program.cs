84a6875c8e9bfce9996dc8f07882d84812adbe7a
lindmark421@gmail.com <lindmark421@gmail.com>
2019-07-29 14:37:59 -0400

    Added Interest Calculator for Savings

--- OOPBAnkingProject/Program.cs
+++ OOPBAnkingProject/Program.cs
@@ -8,28 +8,47 @@ namespace OOPBAnkingProject {
             Savings sav = new Savings();
             sav.Number = "1002";//the .Number is available, but is not in the Savings Class.  That is because it is inherited from Account
             sav.Name = "Savings 1";
+            sav.IntRate = .05;
 
-            sav.Deposit(20);
-            sav.Withdraw(10);
-
+            sav.Deposit(200);
+            sav.Withdraw(100);
+            decimal interestTobePaid = sav.CalcInterest(6);//number of months of interest
+            sav.PayInterest(interestTobePaid);
             decimal savbal = sav.GetBalance();
             Console.WriteLine($"Saving balance is {savbal}");
 
+            //sav.Deposit(10);//always begin the line with the variable
+            //sav.Withdraw(10);
+            //decimal balance = sav.GetBalance();
+            //Console.WriteLine($"Account Balance is {balance} (should be 10)");//added the (should be 10) to verify result
+
+            //sav.Deposit(200);
+            //balance = sav.GetBalance();
+            //Console.WriteLine($"Account Balance is {balance} (should be 10)");
+
+            //sav.Withdraw(100);
+            //balance = sav.GetBalance();
+            //Console.WriteLine($"Account Balance is {balance} (should be 10)");
+
+            //sav.Withdraw(35);
+            //balance = sav.GetBalance();
+            //Console.WriteLine($"Account Balance is {balance}");
 
 
-/*
-            Account acct = new Account();// creates a new account instance with Default Constructor
-            acct.Number = "1001";    //establishes a property as an instance using the 'variable.property name'
-            acct.Name = "Test Account";//we have just established the three properties
 
-            acct.Deposit(10);//always begin the line with the variable
-            acct.Withdraw(10);
-            decimal balance = acct.GetBalance();
-            Console.WriteLine($"Account Balance is {balance} (should be 10)");//added the (should be 10) to verify result
+            /*
+                        Account acct = new Account();// creates a new account instance with Default Constructor
+                        acct.Number = "1001";    //establishes a property as an instance using the 'variable.property name'
+                        acct.Name = "Test Account";//we have just established the three properties
 
-            acct.Deposit(-10);
-            balance = acct.GetBalance();
-            Console.WriteLine($"Account Balance is {balance} (should be 10)");*/
+                        acct.Deposit(10);//always begin the line with the variable
+                        acct.Withdraw(10);
+                        decimal balance = acct.GetBalance();
+                        Console.WriteLine($"Account Balance is {balance} (should be 10)");//added the (should be 10) to verify result
+
+                        acct.Deposit(-10);
+                        balance = acct.GetBalance();
+                        Console.WriteLine($"Account Balance is {balance} (should be 10)");*/
         }
     }
 }
--- OOPBAnkingProject/Savings.cs
+++ OOPBAnkingProject/Savings.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace OOPBAnkingProject {
     public class Savings: Account {// the : Account means that the Account Class in inherited to Savings.  
-                                     //All 'Public' from Account is available
+                                   //All 'Public' from Account is available
+        public double IntRate { get; set; } = 0.03;
+
+        public void  PayInterest(decimal amountofInterest) {
+            this.Deposit(amountofInterest);
+        }
+        public decimal CalcInterest(int months) {
+            double interestTobePaid = 
+                this.IntRate / 12 * months * (double)this.GetBalance();//the (double) is CAST. Temp change to type for GetBalance
+            return (decimal) interestTobePaid;
+        }
+
 
 
     }
