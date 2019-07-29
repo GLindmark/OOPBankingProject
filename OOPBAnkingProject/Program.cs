4e4c2d0868ee6e195f28e5ba34f712b2f1218345
lindmark421@gmail.com <lindmark421@gmail.com>
2019-07-29 14:04:39 -0400

    Added Savings with an Inheritance from Accoun

--- OOPBAnkingProject/Program.cs
+++ OOPBAnkingProject/Program.cs
@@ -5,16 +5,31 @@ namespace OOPBAnkingProject {
 
         static void Main(string[] args) {
 
+            Savings sav = new Savings();
+            sav.Number = "1002";//the .Number is available, but is not in the Savings Class.  That is because it is inherited from Account
+            sav.Name = "Savings 1";
+
+            sav.Deposit(20);
+            sav.Withdraw(10);
+
+            decimal savbal = sav.GetBalance();
+            Console.WriteLine($"Saving balance is {savbal}");
+
+
+
+/*
             Account acct = new Account();// creates a new account instance with Default Constructor
             acct.Number = "1001";    //establishes a property as an instance using the 'variable.property name'
             acct.Name = "Test Account";//we have just established the three properties
+
             acct.Deposit(10);//always begin the line with the variable
             acct.Withdraw(10);
             decimal balance = acct.GetBalance();
             Console.WriteLine($"Account Balance is {balance} (should be 10)");//added the (should be 10) to verify result
+
             acct.Deposit(-10);
             balance = acct.GetBalance();
-            Console.WriteLine($"Account Balance is {balance} (should be 10)");
+            Console.WriteLine($"Account Balance is {balance} (should be 10)");*/
         }
     }
 }
---
+++ OOPBAnkingProject/Savings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPBAnkingProject {
+    public class Savings: Account {// the : Account means that the Account Class in inherited to Savings.  
+                                     //All 'Public' from Account is available
+
+
+    }
+}
