79c5218c269132ded64add5ccd3360d92c1c48b7
lindmark421@gmail.com <lindmark421@gmail.com>
2019-07-29 13:42:57 -0400

    Added Account Tab and tesing information about the account

---
+++ OOPBAnkingProject/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPBAnkingProject {
+
+    public class Account {
+
+        public string Number { get; set; }
+        private decimal Balance { get; set; } = 0;
+        public  string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public void Deposit (decimal Amount) {
+            if (Amount <= 0) {
+                Console.WriteLine($"Deposit Amount must be GT zero");
+            }
+            else {
+                this.Balance += Amount;
+            }
+        }
+        public void Withdraw(decimal Amount) {
+                if(Amount <= 0) {
+                    Console.WriteLine("Withdraw must be GT zero");
+                } else {
+
+                }
+            if (this.Balance >= Amount) {
+                this.Balance -= Amount;
+            }
+        }
+        public decimal GetBalance() {
+            return this.Balance;
+        }
+
+
+    }
+}
--- OOPBAnkingProject/Program.cs
+++ OOPBAnkingProject/Program.cs
@@ -2,8 +2,19 @@
 
 namespace OOPBAnkingProject {
     class Program {
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Account acct = new Account();// creates a new account instance with Default Constructor
+            acct.Number = "1001";    //establishes a property as an instance using the 'variable.property name'
+            acct.Name = "Test Account";//we have just established the three properties
+            acct.Deposit(10);//always begin the line with the variable
+            acct.Withdraw(10);
+            decimal balance = acct.GetBalance();
+            Console.WriteLine($"Account Balance is {balance} (should be 10)");//added the (should be 10) to verify result
+            acct.Deposit(-10);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account Balance is {balance} (should be 10)");
         }
     }
 }
